0832c7d6edb9b4cd0d0db638338e1c681e359260
kotharidarsh <dkothari.c@aes.com>
2025-04-21 19:51:23 +0530

    Optional .env.test file usage when running pytest

--- backend/services/globdoc/tests/conftest.py
+++ backend/services/globdoc/tests/conftest.py
@@ -1,12 +1,19 @@
+from pathlib import Path
 from typing import AsyncGenerator
 
 import pytest
+from dotenv import load_dotenv
 from httpx import AsyncClient
 
-from app import app
+load_dotenv(dotenv_path=Path(__file__).parent / ".env.test", override=True)
+
+from app import app  # noqa: E402
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    """
+    Provides an AsyncClient instance for testing FastAPI app endpoints.
+    """
     async with AsyncClient(app=app, base_url="http://testserver") as client:
         yield client
