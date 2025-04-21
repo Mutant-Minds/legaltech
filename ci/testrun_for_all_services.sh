2f6550a1e93e46df218daafd27772aa200ef6f2f
kotharidarsh <dkothari.c@aes.com>
2025-04-21 17:38:56 +0530

    Reverting addition of a .env file in the services directory

--- ci/testrun_for_all_services.sh
+++ ci/testrun_for_all_services.sh
@@ -6,10 +6,17 @@ EXTRA_ARGUMENTS=("${@:2}")
 
 SERVICES=$(find backend/services -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
 for SERVICE in $SERVICES; do
+  ENV_PATH="backend/services/$SERVICE/.env"
+  if [ ! -f "$ENV_PATH" ]; then
+    echo "Creating empty .env for backend/services/$SERVICE"
+    touch "$ENV_PATH"
+  fi
+
   ENV_TESTING_PATH="backend/services/$SERVICE/.env.testing"
   if [ ! -f "$ENV_TESTING_PATH" ]; then
     echo "Warning: Missing .env.testing in $SERVICE"
   fi
+
   echo "Running '$COMMAND' for service: $SERVICE"
   docker compose run --rm test "$SERVICE" --run "$COMMAND" "${EXTRA_ARGUMENTS[@]}"
 done
